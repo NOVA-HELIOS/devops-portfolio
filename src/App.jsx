ade851d51648072fea0ad512ee32438f9bc8f92a
KOUJULURI VEERA BHASKARA DURGA PRASAD <kpurushotham9000@gmail.com>
2026-07-21 15:23:08 +0530

    Refactor App component to include Loader, Navbar, and Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import Loader from "./components/Loader/Loader";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+
 function App() {
   return (
-    <div className="min-h-screen bg-background text-white">
-      <h1 className="text-5xl font-bold text-center pt-20 text-primary">
-        DevOps Portfolio
-      </h1>
-
-      <p className="text-center text-gray-300 mt-6">
-        Veera Bhaskara Durga Prasad
-      </p>
-    </div>
+    <>
+      <Loader />
+      <Navbar />
+      <Hero />
+    </>
   );
 }
 
